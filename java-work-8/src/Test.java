c7354b43166fb05b2b50a62e2f0f466e3226d3af
rookiesong <songyunxiang1997@163.com>
2018-05-14 23:09:53 +0800

    new eighth push

--- java-work-8/src/Test.java
+++ java-work-8/src/Test.java
@@ -8,13 +8,9 @@ public class Test
         Complex one=new Complex(input.nextDouble(),input.nextDouble());
         System.out.print("请分别输入另一个复数的实部和虚部:");
         Complex another=new Complex(input.nextDouble(),input.nextDouble());
-        Complex add=one.add(another);
-        Complex substract=one.substract(another);
-        Complex multiply=one.multiply(another);
-        Complex divide=one.divide(another);
-        System.out.println("二者之和为："+add.toString());
-        System.out.println("二者之差为："+substract.toString());
-        System.out.println("二者之积为："+multiply.toString());
-        System.out.println("二者之商为："+divide.toString());
+        System.out.println("二者之和为："+one.add(another).toString());
+        System.out.println("二者之差为："+one.substract(another).toString());
+        System.out.println("二者之积为："+one.multiply(another).toString());
+        System.out.println("二者之商为："+one.divide(another).toString());
     }
 }
