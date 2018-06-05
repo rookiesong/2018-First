2ac70bacbc534d6ca10038d4295cd43bf7188727
rookiesong <songyunxiang1997@163.com>
2018-06-05 16:50:05 +0800

    new push

---
+++ Wechat-Projects/Voting-System/pages/index/index.js
@@ -0,0 +1,89 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    votes: '',
+    totalPageNum: 0,
+    currentPage: 1,
+    bottomLineState: false,
+    votedColor: ['#9dc8c8', '#58c9b9', '#519d9e', '#d1b6e1'],
+    filterName: 'all',
+    delOpState: false,
+    pullDownState: true,
+    voteState: true
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  },
+  getVoteListByType: function (e) {
+    let self = this;
+    let curName = e.currentTarget.dataset.src;
+    if (curName) {
+      wx.pageScrollTo({
+        scrollTop: 0,
+        duration: 100
+      })
+      self.setData({
+        filterName: curName,
+        currentPage: 1,
+        bottomLineState: false
+      })
+      self.getVoteList('', 1, curName);
+    }
+  },
+})
