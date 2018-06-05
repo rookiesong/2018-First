4965f81afbc71caef32ab26dbefb2c37013f9381
rookiesong <songyunxiang1997@163.com>
2018-06-05 18:47:04 +0800

    new push

--- C++-Projects/Unit-Converter/Unit-Converter/Unit-Converter/Unit.cpp
+++ C++-Projects/Unit-Converter/Unit-Converter/Unit-Converter/Unit.cpp
@@ -32,6 +32,15 @@ void Value_Unit::Weight()
 	case 0:kgToothers(); break;
 	case 1:gToothers(); break;
 	case 2:mgToothers(); break;
+	case 3:tToothers(); break;
+	case 4:qToothers(); break;
+	case 5:ctToothers(); break;
+	case 6:pointToothers(); break;
+	case 7:lbToothers(); break;
+	case 8:ozToothers(); break;
+	case 9:grToothers(); break;
+	case 10:stToothers(); break;
+	case 11:drToothers(); break;
 	default:
 		break;
 	}
@@ -93,18 +102,246 @@ void Value_Unit::mgToothers()
 			break;
 	switch (i)
 	{
-	case 0:std::cout << this->value << "mg=" << this->value / kgTog << this->new_unit << std::endl; break;
-	case 1:std::cout << this->value << "mg" << this->value / kgTog*kgTomg << this->new_unit << std::endl; break;
-	case 3:std::cout << this->value << "mg" << this->value / kgTog*kgTot << this->new_unit << std::endl; break;
-	case 4:std::cout << this->value << "mg" << this->value / kgTog*kgToq << this->new_unit << std::endl; break;
-	case 5:std::cout << this->value << "mg" << this->value / kgTog*kgToct << this->new_unit << std::endl; break;
-	case 6:std::cout << this->value << "mg" << this->value / kgTog*kgTopoint << this->new_unit << std::endl; break;
-	case 7:std::cout << this->value << "mg" << this->value / kgTog*kgTolb << this->new_unit << std::endl; break;
-	case 8:std::cout << this->value << "mg" << this->value / kgTog*kgTooz << this->new_unit << std::endl; break;
-	case 9:std::cout << this->value << "mg" << this->value / kgTog*kgTogr << this->new_unit << std::endl; break;
-	case 10:std::cout << this->value << "mg=" << this->value / kgTog*kgTost << this->new_unit << std::endl; break;
-	case 11:std::cout << this->value << "mg=" << this->value / kgTog*kgTodr << this->new_unit << std::endl; break;
+	case 0:std::cout << this->value << "mg=" << this->value / kgTomg << this->new_unit << std::endl; break;
+	case 1:std::cout << this->value << "mg=" << this->value / kgTomg*kgTog << this->new_unit << std::endl; break;
+	case 3:std::cout << this->value << "mg=" << this->value / kgTomg*kgTot << this->new_unit << std::endl; break;
+	case 4:std::cout << this->value << "mg=" << this->value / kgTomg*kgToq << this->new_unit << std::endl; break;
+	case 5:std::cout << this->value << "mg=" << this->value / kgTomg*kgToct << this->new_unit << std::endl; break;
+	case 6:std::cout << this->value << "mg=" << this->value / kgTomg*kgTopoint << this->new_unit << std::endl; break;
+	case 7:std::cout << this->value << "mg=" << this->value / kgTomg*kgTolb << this->new_unit << std::endl; break;
+	case 8:std::cout << this->value << "mg=" << this->value / kgTomg*kgTooz << this->new_unit << std::endl; break;
+	case 9:std::cout << this->value << "mg=" << this->value / kgTomg*kgTogr << this->new_unit << std::endl; break;
+	case 10:std::cout << this->value << "mg=" << this->value / kgTomg*kgTost << this->new_unit << std::endl; break;
+	case 11:std::cout << this->value << "mg=" << this->value / kgTomg*kgTodr << this->new_unit << std::endl; break;
 	default:
 		break;
 	}
 }
+
+void Value_Unit::tToothers()
+{
+	int i;
+	for (i = 0; i < 12; i++)
+		if (weightunit[i] == this->new_unit)
+			break;
+	switch (i)
+	{
+	case 0:std::cout << this->value << "t=" << this->value / kgTot << this->new_unit << std::endl; break;
+	case 1:std::cout << this->value << "t=" << this->value / kgTot*kgTog << this->new_unit << std::endl; break;
+	case 2:std::cout << this->value << "t=" << this->value / kgTot*kgTomg << this->new_unit << std::endl; break;
+	case 4:std::cout << this->value << "t=" << this->value / kgTot*kgToq << this->new_unit << std::endl; break;
+	case 5:std::cout << this->value << "t=" << this->value / kgTot*kgToct << this->new_unit << std::endl; break;
+	case 6:std::cout << this->value << "t=" << this->value / kgTot*kgTopoint << this->new_unit << std::endl; break;
+	case 7:std::cout << this->value << "t=" << this->value / kgTot*kgTolb << this->new_unit << std::endl; break;
+	case 8:std::cout << this->value << "t=" << this->value / kgTot*kgTooz << this->new_unit << std::endl; break;
+	case 9:std::cout << this->value << "t=" << this->value / kgTot*kgTogr << this->new_unit << std::endl; break;
+	case 10:std::cout << this->value << "t=" << this->value / kgTot*kgTost << this->new_unit << std::endl; break;
+	case 11:std::cout << this->value << "t=" << this->value / kgTot*kgTodr << this->new_unit << std::endl; break;
+	default:
+		break;
+	}
+}
+
+void Value_Unit::qToothers()
+{
+	int i;
+	for (i = 0; i < 12; i++)
+		if (weightunit[i] == this->new_unit)
+			break;
+	switch (i)
+	{
+	case 0:std::cout << this->value << "q=" << this->value / kgToq << this->new_unit << std::endl; break;
+	case 1:std::cout << this->value << "q=" << this->value / kgToq*kgTog << this->new_unit << std::endl; break;
+	case 2:std::cout << this->value << "q=" << this->value / kgToq*kgTomg << this->new_unit << std::endl; break;
+	case 3:std::cout << this->value << "q=" << this->value / kgToq*kgTot << this->new_unit << std::endl; break;
+	case 5:std::cout << this->value << "q=" << this->value / kgToq*kgToct << this->new_unit << std::endl; break;
+	case 6:std::cout << this->value << "q=" << this->value / kgToq*kgTopoint << this->new_unit << std::endl; break;
+	case 7:std::cout << this->value << "q=" << this->value / kgToq*kgTolb << this->new_unit << std::endl; break;
+	case 8:std::cout << this->value << "q=" << this->value / kgToq*kgTooz << this->new_unit << std::endl; break;
+	case 9:std::cout << this->value << "q=" << this->value / kgToq*kgTogr << this->new_unit << std::endl; break;
+	case 10:std::cout << this->value << "q=" << this->value / kgToq*kgTost << this->new_unit << std::endl; break;
+	case 11:std::cout << this->value << "q=" << this->value / kgToq*kgTodr << this->new_unit << std::endl; break;
+	default:
+		break;
+	}
+}
+
+void Value_Unit::ctToothers()
+{
+	int i;
+	for (i = 0; i < 12; i++)
+		if (weightunit[i] == this->new_unit)
+			break;
+	switch (i)
+	{
+	case 0:std::cout << this->value << "ct=" << this->value / kgToct << this->new_unit << std::endl; break;
+	case 1:std::cout << this->value << "ct=" << this->value / kgToct*kgTog << this->new_unit << std::endl; break;
+	case 2:std::cout << this->value << "ct=" << this->value / kgToct*kgTomg << this->new_unit << std::endl; break;
+	case 3:std::cout << this->value << "ct=" << this->value / kgToct*kgTot << this->new_unit << std::endl; break;
+	case 4:std::cout << this->value << "ct=" << this->value / kgToct*kgToq << this->new_unit << std::endl; break;
+	case 6:std::cout << this->value << "ct=" << this->value / kgToct*kgTopoint << this->new_unit << std::endl; break;
+	case 7:std::cout << this->value << "ct=" << this->value / kgToct*kgTolb << this->new_unit << std::endl; break;
+	case 8:std::cout << this->value << "ct=" << this->value / kgToct*kgTooz << this->new_unit << std::endl; break;
+	case 9:std::cout << this->value << "ct=" << this->value / kgToct*kgTogr << this->new_unit << std::endl; break;
+	case 10:std::cout << this->value << "ct=" << this->value / kgToct*kgTost << this->new_unit << std::endl; break;
+	case 11:std::cout << this->value << "ct=" << this->value / kgToct*kgTodr << this->new_unit << std::endl; break;
+	default:
+		break;
+	}
+}
+
+void Value_Unit::pointToothers()
+{
+	int i;
+	for (i = 0; i < 12; i++)
+		if (weightunit[i] == this->new_unit)
+			break;
+	switch (i)
+	{
+	case 0:std::cout << this->value << "point=" << this->value / kgTopoint << this->new_unit << std::endl; break;
+	case 1:std::cout << this->value << "point=" << this->value / kgTopoint*kgTog << this->new_unit << std::endl; break;
+	case 2:std::cout << this->value << "point=" << this->value / kgTopoint*kgTomg << this->new_unit << std::endl; break;
+	case 3:std::cout << this->value << "point=" << this->value / kgTopoint*kgTot << this->new_unit << std::endl; break;
+	case 4:std::cout << this->value << "point=" << this->value / kgTopoint*kgToq << this->new_unit << std::endl; break;
+	case 5:std::cout << this->value << "point=" << this->value / kgTopoint*kgToct << this->new_unit << std::endl; break;
+	case 7:std::cout << this->value << "point=" << this->value / kgTopoint*kgTolb << this->new_unit << std::endl; break;
+	case 8:std::cout << this->value << "point=" << this->value / kgTopoint*kgTooz << this->new_unit << std::endl; break;
+	case 9:std::cout << this->value << "point=" << this->value / kgTopoint*kgTogr << this->new_unit << std::endl; break;
+	case 10:std::cout << this->value << "point=" << this->value / kgTopoint*kgTost << this->new_unit << std::endl; break;
+	case 11:std::cout << this->value << "point=" << this->value / kgTopoint*kgTodr << this->new_unit << std::endl; break;
+	default:
+		break;
+	}
+}
+
+void Value_Unit::lbToothers()
+{
+	int i;
+	for (i = 0; i < 12; i++)
+		if (weightunit[i] == this->new_unit)
+			break;
+	switch (i)
+	{
+	case 0:std::cout << this->value << "lb=" << this->value / kgTolb << this->new_unit << std::endl; break;
+	case 1:std::cout << this->value << "lb=" << this->value / kgTolb*kgTog << this->new_unit << std::endl; break;
+	case 2:std::cout << this->value << "lb=" << this->value / kgTolb*kgTomg << this->new_unit << std::endl; break;
+	case 3:std::cout << this->value << "lb=" << this->value / kgTolb*kgTot << this->new_unit << std::endl; break;
+	case 4:std::cout << this->value << "lb=" << this->value / kgTolb*kgToq << this->new_unit << std::endl; break;
+	case 5:std::cout << this->value << "lb=" << this->value / kgTolb*kgToct << this->new_unit << std::endl; break;
+	case 6:std::cout << this->value << "lb=" << this->value / kgTolb*kgTopoint << this->new_unit << std::endl; break;
+	case 8:std::cout << this->value << "lb=" << this->value / kgTolb*kgTooz << this->new_unit << std::endl; break;
+	case 9:std::cout << this->value << "lb=" << this->value / kgTolb*kgTogr << this->new_unit << std::endl; break;
+	case 10:std::cout << this->value << "lb=" << this->value / kgTolb*kgTost << this->new_unit << std::endl; break;
+	case 11:std::cout << this->value << "lb=" << this->value / kgTolb*kgTodr << this->new_unit << std::endl; break;
+	default:
+		break;
+	}
+}
+
+void Value_Unit::ozToothers()
+{
+	int i;
+	for (i = 0; i < 12; i++)
+		if (weightunit[i] == this->new_unit)
+			break;
+	switch (i)
+	{
+	case 0:std::cout << this->value << "oz=" << this->value / kgTooz << this->new_unit << std::endl; break;
+	case 1:std::cout << this->value << "oz=" << this->value / kgTooz*kgTog << this->new_unit << std::endl; break;
+	case 2:std::cout << this->value << "oz=" << this->value / kgTooz*kgTomg << this->new_unit << std::endl; break;
+	case 3:std::cout << this->value << "oz=" << this->value / kgTooz*kgTot << this->new_unit << std::endl; break;
+	case 4:std::cout << this->value << "oz=" << this->value / kgTooz*kgToq << this->new_unit << std::endl; break;
+	case 5:std::cout << this->value << "oz=" << this->value / kgTooz*kgToct << this->new_unit << std::endl; break;
+	case 6:std::cout << this->value << "oz=" << this->value / kgTooz*kgTopoint << this->new_unit << std::endl; break;
+	case 7:std::cout << this->value << "oz=" << this->value / kgTooz*kgTolb << this->new_unit << std::endl; break;
+	case 9:std::cout << this->value << "oz=" << this->value / kgTooz*kgTogr << this->new_unit << std::endl; break;
+	case 10:std::cout << this->value << "oz=" << this->value / kgTooz*kgTost << this->new_unit << std::endl; break;
+	case 11:std::cout << this->value << "oz=" << this->value / kgTooz*kgTodr << this->new_unit << std::endl; break;
+	default:
+		break;
+	}
+}
+
+void Value_Unit::grToothers()
+{
+	int i;
+	for (i = 0; i < 12; i++)
+		if (weightunit[i] == this->new_unit)
+			break;
+	switch (i)
+	{
+	case 0:std::cout << this->value << "gr=" << this->value / kgTogr << this->new_unit << std::endl; break;
+	case 1:std::cout << this->value << "gr=" << this->value / kgTogr*kgTog << this->new_unit << std::endl; break;
+	case 2:std::cout << this->value << "gr=" << this->value / kgTogr*kgTomg << this->new_unit << std::endl; break;
+	case 3:std::cout << this->value << "gr=" << this->value / kgTogr*kgTot << this->new_unit << std::endl; break;
+	case 4:std::cout << this->value << "gr=" << this->value / kgTogr*kgToq << this->new_unit << std::endl; break;
+	case 5:std::cout << this->value << "gr=" << this->value / kgTogr*kgToct << this->new_unit << std::endl; break;
+	case 6:std::cout << this->value << "gr=" << this->value / kgTogr*kgTopoint << this->new_unit << std::endl; break;
+	case 7:std::cout << this->value << "gr=" << this->value / kgTogr*kgTolb << this->new_unit << std::endl; break;
+	case 8:std::cout << this->value << "gr=" << this->value / kgTogr*kgTooz << this->new_unit << std::endl; break;
+	case 10:std::cout << this->value << "gr=" << this->value / kgTogr*kgTost << this->new_unit << std::endl; break;
+	case 11:std::cout << this->value << "gr=" << this->value / kgTogr*kgTodr << this->new_unit << std::endl; break;
+	default:
+		break;
+	}
+}
+
+void Value_Unit::stToothers()
+{
+	int i;
+	for (i = 0; i < 12; i++)
+		if (weightunit[i] == this->new_unit)
+			break;
+	switch (i)
+	{
+	case 0:std::cout << this->value << "st=" << this->value / kgTost << this->new_unit << std::endl; break;
+	case 1:std::cout << this->value << "st=" << this->value / kgTost*kgTog << this->new_unit << std::endl; break;
+	case 2:std::cout << this->value << "st=" << this->value / kgTost*kgTomg << this->new_unit << std::endl; break;
+	case 3:std::cout << this->value << "st=" << this->value / kgTost*kgTot << this->new_unit << std::endl; break;
+	case 4:std::cout << this->value << "st=" << this->value / kgTost*kgToq << this->new_unit << std::endl; break;
+	case 5:std::cout << this->value << "st=" << this->value / kgTost*kgToct << this->new_unit << std::endl; break;
+	case 6:std::cout << this->value << "st=" << this->value / kgTost*kgTopoint << this->new_unit << std::endl; break;
+	case 7:std::cout << this->value << "st=" << this->value / kgTost*kgTolb << this->new_unit << std::endl; break;
+	case 8:std::cout << this->value << "st=" << this->value / kgTost*kgTooz << this->new_unit << std::endl; break;
+	case 9:std::cout << this->value << "st=" << this->value / kgTost*kgTogr << this->new_unit << std::endl; break;
+	case 11:std::cout << this->value << "st=" << this->value / kgTost*kgTodr << this->new_unit << std::endl; break;
+	default:
+		break;
+	}
+}
+
+void Value_Unit::drToothers()
+{
+	int i;
+	for (i = 0; i < 12; i++)
+		if (weightunit[i] == this->new_unit)
+			break;
+	switch (i)
+	{
+	case 0:std::cout << this->value << "dr=" << this->value / kgTodr << this->new_unit << std::endl; break;
+	case 1:std::cout << this->value << "dr=" << this->value / kgTodr*kgTog << this->new_unit << std::endl; break;
+	case 2:std::cout << this->value << "dr=" << this->value / kgTodr*kgTomg << this->new_unit << std::endl; break;
+	case 3:std::cout << this->value << "dr=" << this->value / kgTodr*kgTot << this->new_unit << std::endl; break;
+	case 4:std::cout << this->value << "dr=" << this->value / kgTodr*kgToq << this->new_unit << std::endl; break;
+	case 5:std::cout << this->value << "dr=" << this->value / kgTodr*kgToct << this->new_unit << std::endl; break;
+	case 6:std::cout << this->value << "dr=" << this->value / kgTodr*kgTopoint << this->new_unit << std::endl; break;
+	case 7:std::cout << this->value << "dr=" << this->value / kgTodr*kgTolb << this->new_unit << std::endl; break;
+	case 8:std::cout << this->value << "dr=" << this->value / kgTodr*kgTooz << this->new_unit << std::endl; break;
+	case 9:std::cout << this->value << "dr=" << this->value / kgTodr*kgTogr << this->new_unit << std::endl; break;
+	case 10:std::cout << this->value << "dr=" << this->value / kgTodr*kgTost << this->new_unit << std::endl; break;
+	default:
+		break;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
