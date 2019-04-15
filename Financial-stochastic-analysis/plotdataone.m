7dbf452514b0ace632a9291173942e2427f06809
rookiesong <32297925+rookiesong@users.noreply.github.com>
2019-04-15 21:53:25 +0800

    new projects

---
+++ Financial-stochastic-analysis/plotdataone.m
@@ -0,0 +1,9 @@
+filename = 'plotdata.txt';
+[N,V,Time]=textread(filename,'%n%n%n',19);
+plot(N,V,'k*','LineWidth',1.5);
+hold on
+plot(N,V,'r','LineWidth',1.5);
+xlabel('N');
+ylabel('V');
+title('Option price V under different N');
+hold off
