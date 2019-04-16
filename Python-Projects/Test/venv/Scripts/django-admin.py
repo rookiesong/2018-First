216711b018479a226695df4534225e33c535cf85
rookiesong <32297925+rookiesong@users.noreply.github.com>
2019-04-16 18:26:30 +0800

    delete

--- Python-Projects/Test/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\Documents\GitHub\First\Python-Projects\Test\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
